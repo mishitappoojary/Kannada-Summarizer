d800e947c5922594ab428c2d7de0806b706fe840
mishitappoojary <142720289+mishitappoojary@users.noreply.github.com>
2024-09-17 18:58:23 +0530

    Update extractive_summary.py

--- extractive_summary.py
+++ extractive_summary.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import nltk
+nltk.download('punkt_tab')
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.tokenize import sent_tokenize, word_tokenize
 import pickle
